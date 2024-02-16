6611bac5782050c1ef5e6fa1e8d15a46dc7766cc
Porter Lyman <dev@porterlyman.com>
2024-02-16 00:09:37 -0700

    Initial commit done entirely from a phone

---
+++ src/app/components/set-overview/set-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-set-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './set-overview.component.html',
+  styleUrl: './set-overview.component.scss'
+})
+export class SetOverviewComponent {
+
+}
