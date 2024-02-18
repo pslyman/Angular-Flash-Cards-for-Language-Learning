b6a13e7ab802f8aee85e9988bfe47b9c8b16493b
pslyman <dev@porterlyman.com>
2024-02-17 22:40:46 -0700

    merge in personal old sets

--- src/app/constants/sets.constants.ts
+++ src/app/constants/sets.constants.ts
@@ -2,37 +2,358 @@ import { Set } from "../interfaces/sets.interface";
 
 export const cardSets: Set[] = [
   {
-    id: 1,
-    name: "Hangeul",
-    description: "Review your Korean alphabet and sounds",
+    id: 9,
+    name: "Grammar - Beginner 1",
+    description: "",
     cards: [
       {
-        front: "",
-        back: "",
+        front: "안녕하세요",
+        back: "Hello. \n\n안녕 = well-being, peace, health. \n하세요 = you do, do you?, please do. \n",
+        extra: "",
+      },
+      {
+        front: "감사합니다",
+        back: "Thank you. \n\n감사 = appreciation, thankfulness, gratitude. \n합니다 = I do, I am doing. ",
+        extra: "",
+      },
+      {
+        front: "네",
+        back: "I agree. Affirmative. Yes? Uh huh. Yeah. ",
+        extra: "",
+      },
+      {
+        front: "아니요",
+        back: "No. I disagree. ",
+        extra: "",
+      },
+      {
+        front: "맞아요",
+        back: 'Can be used after 네 to say "Yes, you are right". ',
+        extra: "",
+      },
+      {
+        front: "안녕히 계세요",
+        back: "Stay in peace (Goodbye)",
+        extra: "",
+      },
+      {
+        front: "안녕히 가세요",
+        back: "Go in peace (Goodbye)",
+        extra: "",
+      },
+      {
+        front: "죄송합니다",
+        back: "I apologize (chweh-song hamnida)",
+        extra: "",
+      },
+      {
+        front: "저기요",
+        back: "(excuse me) Hey you, 'over there!' - politely",
+        extra: "",
+      },
+      {
+        front: "잠시만요",
+        back: "Just a second (passing through crowd bumping into people)",
         extra: "",
       },
     ],
   },
   {
-    id: 2,
-    name: "Hangeul Batchim",
-    description: "Review Korean final consonants",
+    id: 10,
+    name: "Grammar - Beginner 2",
+    description: "",
     cards: [
       {
-        front: "",
-        back: "",
+        front: "이거 (in 이거 뭐예요)",
+        back: 'This - "what is THIS?" Also 이거 물이에요 - "This is water"',
+        extra: "",
+      },
+      {
+        front: "뭐 in 뭐예요",
+        back: "What? As in what is it?",
+        extra: "",
+      },
+      {
+        front: "이에요 and 예요",
+        back: '"it is" or "is it"',
+        extra: "",
+      },
+      {
+        front: "이",
+        back: "This (near you)",
+        extra: "",
+      },
+      {
+        front: "저",
+        back: "That (over there)",
+        extra: "",
+      },
+      {
+        front: "그",
+        back: "The / that (near the other person)",
+        extra: "",
+      },
+      {
+        front: "아니에요 ",
+        back: "To be not (polite, present tense). Can also be said in response to 'thank you'. ",
+        extra: "",
+      },
+      {
+        front: "은/는",
+        back: "Marks the topic of the sentence, and puts emphasis on it. 이 책은 = This book (topic of this sentence)",
+        extra: "",
+      },
+    ],
+  },
+  {
+    id: 1,
+    name: "Hangeul Level 1",
+    description: "",
+    cards: [
+      {
+        front: "What sound does 으 make?",
+        back: 'Neutral (unhappy face) sound, "eu". ',
+        extra: "",
+      },
+      {
+        front: "What sound does 오 make? ",
+        back: 'The "o" sound from "old". ',
+        extra: "",
+      },
+      {
+        front: "What sound does 우 make? ",
+        back: '"Ooh"',
+        extra: "",
+      },
+      {
+        front: "What sound does 이 make?",
+        back: '"ee"',
+        extra: "",
+      },
+      {
+        front: "What sound does 어 make?",
+        back: '"uh", or eo',
+        extra: "",
+      },
+      {
+        front: "What sound does 아 make?",
+        back: '"ah"',
+        extra: "",
+      },
+      {
+        front: "What does ㅇ do?",
+        back: "Place holder consonant, makes no sound. Turns a vowel into a full character by being a pair. ",
+        extra: "",
+      },
+      {
+        front: "What does ㅣ represent? ",
+        back: "Human beings",
+        extra: "",
+      },
+      {
+        front: "What does . represent?",
+        back: "Sky",
+        extra: "",
+      },
+      {
+        front: "What does ㅡ represent?",
+        back: "Ground",
+        extra: "",
+      },
+      {
+        front: "How do you Koreanize English words?",
+        back: "Add ㅡ to the end. ",
         extra: "",
       },
     ],
   },
   {
     id: 3,
-    name: "Grammar - Beginner",
-    description: "Basic words and sentence structure",
+    name: "Hangeul Level 2",
+    description: "",
     cards: [
       {
-        front: "",
-        back: "",
+        front: "What sound does ㄱ make?",
+        back: '"G" or "K", a "clicky" G. \nPush your tongue forward slightly more forward than an English G would normally require. ',
+        extra: "",
+      },
+      {
+        front: "What sound does ㅋ make?",
+        back: 'ㅋ = "K".',
+        extra: "",
+      },
+      {
+        front: "What sound does ㄴ make? ",
+        back: '"N" sound - Tongue bottom of upper teeth. It\'s a "soft N". ',
+        extra: "",
+      },
+      {
+        front: "What sound does ㄷ make?",
+        back: "ㄷ = D/Th (keep tongue on top lower teeth).",
+        extra: "",
+      },
+      {
+        front: "What sound does ㄹ make?",
+        back: 'A rolling "R". Can resemble the "L" sound in the middle of a word. ',
+        extra: "",
+      },
+      {
+        front: "What sound does ㅁ make?",
+        back: '"M"',
+        extra: "",
+      },
+      {
+        front: "What sound does ㅂ make?",
+        back: 'ㅂ = Soft "B".',
+        extra: "",
+      },
+      {
+        front: "What sound does ㄲ make?",
+        back: 'ㄲ = "G".',
+        extra: "",
+      },
+      {
+        front: "What sound does ㄸ make?",
+        back: 'ㄸ = "D". ',
+        extra: "",
+      },
+      {
+        front: "What sound does ㅌ make?",
+        back: 'ㅌ = "T".',
+        extra: "",
+      },
+      {
+        front: "What sound does ㅍ make?",
+        back: 'ㅍ = "P".',
+        extra: "",
+      },
+      {
+        front: "What sound does ㅃ make?",
+        back: 'ㅃ = "B".',
+        extra: "",
+      },
+      {
+        front:
+          "What happens to the sound of a consonant when you add lines to it?",
+        back: "Two things:\n- More friction in the sound.\n- Higher pitch! ",
+        extra: "",
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "Hangeul Level 3",
+    description: "",
+    cards: [
+      {
+        front: "What sound does ㅅ make?",
+        back: '"S" (soft)',
+        extra: "",
+      },
+      {
+        front: "What sound does ㅈ make? ",
+        back: 'ㅈ = between "ch" and "j", light.',
+        extra: "",
+      },
+      {
+        front: "What does ㅎ do when in a syllable block?",
+        back: 'More friction when you let air through your mouth, like an "H". \n\n오 = eo. \n호 = heo (like o in old). ',
+        extra: "",
+      },
+      {
+        front: "What sound does ㅊ make? ",
+        back: 'ㅊ= added friction and air to ㅈ (between "j" and "ch"). ',
+        extra: "",
+      },
+      {
+        front: "What sound does ㅆ make? ",
+        back: 'ㅆ = "S" (less air escape). ',
+        extra: "",
+      },
+      {
+        front: "What sound does ㅉ make? ",
+        back: 'ㅉ = added friction to (ㅈ) like "t". ',
+        extra: "",
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: "Hangeul Level 4",
+    description: "",
+    cards: [
+      {
+        front: "What sound does ㅘ make? ",
+        back: '"Wah". ',
+        extra: "",
+      },
+      {
+        front: "What sound does ㅝ make?",
+        back: '"Wuh"',
+        extra: "",
+      },
+      {
+        front: "What sound doesㅐmake?",
+        back: 'Ai without a mouth change, like "eh"',
+        extra: "",
+      },
+      {
+        front: "What sound doesㅔmake?",
+        back: 'Ai, or "eh" much like ㅐ(which is more open). ',
+        extra: "",
+      },
+      {
+        front: "What sound does ㅞ make?",
+        back: '"Weh", but with a hint of "ai"',
+        extra: "",
+      },
+      {
+        front: "What sound does ㅚ make?",
+        back: '"Weh" with an "ai" sound',
+        extra: "",
+      },
+      {
+        front: "What sound does ㅙ make?",
+        back: '"Weh", but with a hint of "ai"',
+        extra: "",
+      },
+      {
+        front: "What sound does ㅟ make?",
+        back: '"We"',
+        extra: "",
+      },
+      {
+        front: "What sound does ㅢ make?",
+        back: "This is phonetic, say as seen. ",
+        extra: "",
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: "Hangeul Level 5",
+    description: "",
+    cards: [
+      {
+        front: 'How does "Y" work in Hangeul?',
+        back: 'You can take the following vowels:\nㅏ,ㅓ,ㅗ,ㅜ,ㅐ,ㅔ. \n\nAnd add a line to them:\nㅑ,ㅕ,ㅛ,ㅠ,ㅒ,ㅖ. \n\nWhich adds a "y" sound to the beginning. ㅏ(Ah) would become ㅑ(Yah). \n\n',
+        extra: "",
+      },
+    ],
+  },
+  {
+    id: 8,
+    name: "Hangeul Level 6",
+    description: "",
+    cards: [
+      {
+        front: "What sound does ㅇ make at the end of a block?",
+        back: '"ng" like sting and ring. ',
+        extra: "",
+      },
+      {
+        front: "How do you handle a consonant at the end of a word?",
+        back: "Short stop. Most consonants are said normally at the end, but some are reduced to a simple form. These rules will be covered more in depth in the pronunciation course. ",
         extra: "",
       },
     ],
