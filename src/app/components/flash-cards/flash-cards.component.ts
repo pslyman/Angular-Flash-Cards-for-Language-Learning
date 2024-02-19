922ca4f8b4e52d4cdbdbf4ca25c4d580669d6918
pslyman <dev@porterlyman.com>
2024-02-18 19:47:45 -0700

    feature complete, but could be prettied up more

--- src/app/components/flash-cards/flash-cards.component.ts
+++ src/app/components/flash-cards/flash-cards.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterModule } from '@angular/router';
-import { Card, Set } from '../../interfaces/sets.interface'
-import { cardSets } from '../../constants/sets.constants';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, RouterModule } from "@angular/router";
+import { Card, Set } from "../../interfaces/sets.interface";
+import { cardSets } from "../../constants/sets.constants";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-flash-cards',
+  selector: "app-flash-cards",
   standalone: true,
   imports: [CommonModule, RouterModule],
-  templateUrl: './flash-cards.component.html',
-  styleUrl: './flash-cards.component.scss'
+  templateUrl: "./flash-cards.component.html",
+  styleUrl: "./flash-cards.component.scss",
 })
 export class FlashCardsComponent implements OnInit {
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) {}
 
-  setId = '';
+  setId = "";
   set: Set = {
     id: 0,
-    name: '',
-    description: '',
-    cards: []
+    name: "",
+    description: "",
+    cards: [],
   };
   cards: Card[] = [];
   currentCardIterator = 0;
@@ -29,7 +29,7 @@ export class FlashCardsComponent implements OnInit {
 
   ngOnInit(): void {
     this.setId = this.route.snapshot.paramMap.get("setId") ?? "";
-    const findSet = cardSets.find(set => set.id.toString() === this.setId);
+    const findSet = cardSets.find((set) => set.id.toString() === this.setId);
     if (findSet) {
       this.set = findSet;
     }
@@ -38,14 +38,15 @@ export class FlashCardsComponent implements OnInit {
   newCard(modifier: number) {
     if (this.currentCardIterator + modifier < 0) {
       this.currentCardIterator = 0;
-    } else if (this.currentCardIterator + modifier > this.set.cards.length -1) {
+    } else if (
+      this.currentCardIterator + modifier >
+      this.set.cards.length - 1
+    ) {
       this.currentCardIterator = this.set.cards.length - 1;
-    }
-    else {
-      this.currentCardIterator += modifier
+    } else {
+      this.currentCardIterator += modifier;
     }
 
     this.hideCard = true;
   }
-
 }
